e610399fccc2e6fc240043d7e13e681eaaed8f98
Dave Berzack <dberzack>
2015-02-12 18:29:54 -0500

    ...

--- js/main.js
+++ js/main.js
@@ -21,6 +21,32 @@ var app = {
         }
     },
 
+    changePicture: function () {
+
+        event.preventDefault();
+        if (!navigator.camera) {
+            app.showAlert("Camera API not supported", "Error");
+            return;
+        }
+        var options =   {   quality: 50,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                            encodingType: 0     // 0=JPG 1=PNG
+                        };
+     
+        navigator.camera.getPicture(
+            function(imageData) {
+                $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+            },
+            function() {
+                app.showAlert('Error taking picture', 'Error');
+            },
+            options);
+     
+        return false;
+
+    },
+
     initialize: function() {
         console.log("init");
         var self = this;
@@ -29,6 +55,8 @@ var app = {
         });
 
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+        $(".change-pic-btn").click(self.changePicture);
     }
 
 };
